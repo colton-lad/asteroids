06918b66dfe6464aada570edd92b391d12602b58
colton-lad <colton.lad@gmail.com>
2024-10-14 12:09:47 -0500

    added asteroid-shot collision and destroy functionality

--- main.py
+++ main.py
@@ -42,6 +42,10 @@ def main():
             if asteroid.collision(player):
                 print("Game over!")
                 return
+            for shot in shots:
+                if asteroid.collision(shot):
+                    asteroid.kill()
+                    shot.kill()
 
         for drawable in drawables:
             drawable.draw(screen)
